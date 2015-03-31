7b67af452f14f205e9fb21aff34ee444ffd43c9d
tainh56 <nguyenhuutai1443@gmail.com>
2015-04-01 01:47:16 +0700

    Lam giao dien

--- admin/Admin_ChucNang.php
+++ admin/Admin_ChucNang.php
@@ -5,16 +5,16 @@
     <section class="sidebar">
 
         <!-- Sidebar user panel (optional) -->
-<!--        <div class="user-panel">
-            <div class="pull-left image">
-                <img src="../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image" />
-            </div>
-            <div class="pull-left info">
-                <p>Admin</p>
-                 Status 
-                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-            </div>
-        </div>-->
+        <!--        <div class="user-panel">
+                    <div class="pull-left image">
+                        <img src="../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image" />
+                    </div>
+                    <div class="pull-left info">
+                        <p>Admin</p>
+                         Status 
+                        <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+                    </div>
+                </div>-->
 
         <!-- search form (Optional) -->
         <form action="#" method="get" class="sidebar-form">
@@ -31,13 +31,52 @@
         <ul class="sidebar-menu">
             <li class="header">Chức năng</li>
             <!-- Optionally, you can add icons to the links -->
-            <li class="active"><a href="#"><span>Link</span></a><</li>
-            <li><a href="#"><span>Another Link</span></a></li>
             <li class="treeview">
                 <a href="#"><span>Sản phẩm</span> <i class="fa fa-angle-left pull-right"></i></a>
                 <ul class="treeview-menu">
                     <li><a href="#">Danh sách</a></li>
                     <li><a href="ThemSanPham.php">Thêm mới</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#"><span>Linh kiện</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="ViewLinhKien.php">Danh sách</a></li>
+                    <li><a href="ThemSanPham.php">Thêm mới</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#"><span>Phụ kiện</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="#">Danh sách</a></li>
+                    <li><a href="ThemSanPham.php">Thêm mới</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#"><span>Laptop</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="#">Danh sách</a></li>
+                    <li><a href="ThemSanPham.php">Thêm mới</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#"><span>Smart phone</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="#">Danh sách</a></li>
+                    <li><a href="ThemSanPham.php">Thêm mới</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-share"></i> <span>Sản phẩm</span>
+                    <i class="fa fa-angle-left pull-right"></i>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="#"><i class="fa fa-circle-o"></i>Smart phone</a></li>
+                    <li><a href="#"><i class="fa fa-circle-o"></i>Laptop</a></li>
+                    <li><a href="#"><i class="fa fa-circle-o"></i>Máy bàn</a></li>
+                    <li><a href="#"><i class="fa fa-circle-o"></i>Linh kiện</a></li>
+                    <li><a href="#"><i class="fa fa-circle-o"></i>Phụ kiện</a></li>
                 </ul>
             </li>
         </ul><!-- /.sidebar-menu -->
--- admin/admin.php
+++ admin/admin.php
@@ -5,50 +5,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
 -->
 <html>
     <head>
-        <meta charset="UTF-8">
         <title>Admin</title>
-        <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-        <!-- Bootstrap 3.3.2 -->
-        <link href="../bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css" />
-        <!-- Font Awesome Icons -->
-        <link href="../font-awesome-4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-        <!-- Ionicons -->
-        <link href="http://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css" rel="stylesheet" type="text/css" />
-        <!-- Theme style -->
-        <link href="../dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
-        <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
-              page. However, you can choose any other skin. Make sure you
-              apply the skin class to the body tag so the changes take effect.
-        -->
-        <link href="../dist/css/skins/skin-purple.css" rel="stylesheet" type="text/css" />
-
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-            <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-        <![endif]-->
+        <?php include 'includes/Head.php'; ?>
     </head>
-    <!--
-    BODY TAG OPTIONS:
-    =================
-    Apply one or more of the following classes to get the 
-    desired effect
-    |---------------------------------------------------------|
-    | SKINS         | skin-blue                               |
-    |               | skin-black                              |
-    |               | skin-purple                             |
-    |               | skin-yellow                             |
-    |               | skin-red                                |
-    |               | skin-green                              |
-    |---------------------------------------------------------|
-    |LAYOUT OPTIONS | fixed                                   |
-    |               | layout-boxed                            |
-    |               | layout-top-nav                          |
-    |               | sidebar-collapse                        |  
-    |---------------------------------------------------------|
     
-    -->
     <body class="skin-purple">
         <div class="wrapper">
             <?php include '../includes/Header.php'; ?>
@@ -80,17 +40,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
         </div><!-- ./wrapper -->
 
-        <!-- REQUIRED JS SCRIPTS -->
-
-        <!-- jQuery 2.1.3 -->
-        <script src="../plugins/jQuery/jQuery-2.1.3.min.js"></script>
-        <!-- Bootstrap 3.3.2 JS -->
-        <script src="../bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-        <!-- AdminLTE App -->
-        <script src="../dist/js/app.min.js" type="text/javascript"></script>
-
-        <!-- Optionally, you can add Slimscroll and FastClick plugins. 
-              Both of these plugins are recommended to enhance the 
-              user experience -->
+        <?php include 'includes/Script.php'; ?>
     </body>
 </html>
